526f56d517830cf3e6cdaef06e18d25907b2ec7d
caio900 <caiomarteloni@gmail.com>
2024-11-19 10:02:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+ 
+ function cabeça(){
+ alert("Em pokemon x e y Froakie inicial de água começa com os ataques Pound, Growl e Bubble.");
+ }
+ 
+ function frobolhas(){
+    alert("Froakie usa suas frobolhas como proteção e pode lançar elas pois, elas grudão em quase qualquer coisa.");
+ }
+
+ function corpo(){
+    alert("Froakie e o primeiro inicial de água focado em velocidade.");
+ }
